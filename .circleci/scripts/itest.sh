bd3b84f4ffb44b2e4182d074943889018c8410d2
Jesse White <jesse@opennms.org>
2019-04-07 21:19:23 -0400

    Add integration tests.

---
+++ .circleci/scripts/itest.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -e
+echo "#### Allowing non-root ICMP"
+sudo sysctl net.ipv4.ping_group_range='0 429496729'
+
+echo "#### Setting up Postgres"
+docker run --rm --name postgres-onms-itest -p 5432:5432 -d postgres:9.5.1
+
+echo "#### Executing tests"
+cd ~/project
+pyenv local 3.5.2
+MODULES_TO_TEST=$(python3 .circleci/scripts/find-tests.py . | circleci tests split | paste -s -d, -)
+echo "Building modules and dependencies for: ${MODULES_TO_TEST}"
+mvn -P'!checkstyle' -P'!enable.tarball' -DupdatePolicy=never -Dbuild.skip.tarball=true -DskipTests=true -am -pl ${MODULES_TO_TEST} install
+echo "Build complete. Verifying..."
+mvn -P'!checkstyle' -DupdatePolicy=never -DskipITs=false -DskipTests=false --batch-mode -pl ${MODULES_TO_TEST} verify
+
